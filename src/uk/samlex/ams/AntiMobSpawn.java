0e8da884802f70a826f76f46ffbd0134d33db15c
ejhunter <ejhunter@cablecity>
2014-05-27 19:25:39 +0100

    Setup main class, adding database setup and other setup

--- src/uk/samlex/ams/AntiMobSpawn.java
+++ src/uk/samlex/ams/AntiMobSpawn.java
@@ -1,7 +1,46 @@
 package uk.samlex.ams;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.PersistenceException;
+
 import org.bukkit.plugin.java.JavaPlugin;
+
+import uk.samlex.ams.config.WorldZone;
+import uk.samlex.ams.config.ConfigStore;
+import uk.samlex.ams.event.EntitySpawnHandler;
 
 public class AntiMobSpawn extends JavaPlugin {
 
+    private static AntiMobSpawn INSTANCE;
+
+    @Override
+    public void onEnable() {
+        INSTANCE = this;
+
+        new ConfigStore();
+
+        getServer().getPluginManager().registerEvents(new EntitySpawnHandler(), instance());
+    }
+    
+    public static AntiMobSpawn instance() {
+        return INSTANCE;
+    }
+
+    public void checkDatabase() {
+        try {
+            getDatabase().find(WorldZone.class).findRowCount();
+        } catch (PersistenceException pe) {
+            getLogger().info("Installing database due to first time use");
+            installDDL();
+        }
+    }
+
+    @Override
+    public List<Class<?>> getDatabaseClasses() {
+        ArrayList<Class<?>> dbClasses = new ArrayList<Class<?>>(1);
+        dbClasses.add(WorldZone.class);
+        return dbClasses;
+    }
 }
