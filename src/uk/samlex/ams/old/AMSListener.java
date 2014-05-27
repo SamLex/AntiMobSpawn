54123f916e5c08baa68a5985886f9f2116a524a5
ejhunter <ejhunter@cablecity>
2014-05-27 16:14:04 +0100

    Here we go again - Rewrite for 1.7

--- src/uk/samlex/ams/old/AMSListener.java
+++ src/uk/samlex/ams/old/AMSListener.java
@@ -16,7 +16,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package uk.samlex.ams;
+package uk.samlex.ams.old;
 
 import java.util.List;
 
--- src/uk/samlex/ams/prototype/AntiMobSpawn.java
+++
@@ -1,7 +0,0 @@
-package uk.samlex.ams.prototype;
-
-import org.bukkit.plugin.java.JavaPlugin;
-
-public class AntiMobSpawn extends JavaPlugin {
-
-}
