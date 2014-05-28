50cb3917c2f6620e55fda0660547a22183e797b0
ejhunter <ejhunter@cablecity>
2014-05-28 16:32:58 +0100

    Added partial argument comparison for tab completion

--- src/uk/samlex/ams/command/GenericCommand.java
+++ src/uk/samlex/ams/command/GenericCommand.java
@@ -1,8 +1,20 @@
 package uk.samlex.ams.command;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.TabCompleter;
 
 public abstract class GenericCommand implements CommandExecutor, TabCompleter {
 
+    protected static List<String> checkPartialArgument(String partialArgument, String[] possiblities) {
+        ArrayList<String> morePossible = new ArrayList<>();
+        for (String s : possiblities) {
+            if (partialArgument.length() < s.length())
+                if (partialArgument.startsWith(s.substring(0, partialArgument.length())))
+                    morePossible.add(s);
+        }
+        return morePossible;
+    }
 }
